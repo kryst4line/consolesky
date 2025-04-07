58cfc9bb9a42990c265ebd2dba709bc753671d8b
kbenlloch <kbenlloch@biit-solutions.com>
2025-04-07 13:44:15 +0200

    feat: list trending topics on auxbar

--- src/app/components/navigation/auxbar/auxbar.component.ts
+++ src/app/components/navigation/auxbar/auxbar.component.ts
@@ -1,5 +1,9 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, signal} from '@angular/core';
 import {PostService} from '@services/post.service';
+import {from} from 'rxjs';
+import {agent} from '@core/bsky.api';
+import {AuthService} from '@core/auth/auth.service';
+import type * as AppBskyUnspeccedDefs from '@atproto/api/src/client/types/app/bsky/unspecced/defs';
 
 @Component({
   selector: 'auxbar',
@@ -8,8 +12,25 @@ import {PostService} from '@services/post.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuxbarComponent {
-  constructor(
-    protected postService: PostService
-  ) {}
+  topics = signal<AppBskyUnspeccedDefs.TrendingTopic[]>([]);
 
+  constructor(
+    protected postService: PostService,
+    private authService: AuthService,
+    private cdRef: ChangeDetectorRef
+  ) {
+    this.loadTopics();
+  }
+
+  loadTopics() {
+    from(agent.app.bsky.unspecced.getTrendingTopics({
+      viewer: this.authService.loggedUser().did,
+      limit: 10
+    })).subscribe({
+      next: response => {
+        this.topics.set(response.data.topics);
+        this.cdRef.markForCheck();
+      }
+    })
+  }
 }
